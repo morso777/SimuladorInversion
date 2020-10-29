c60a9eb02d9d81302ba528d025088b720839acf3
hiran peña toledo <vohiran@hotmail.com>
2020-10-29 17:26:10 -0600

    4 commit

--- src/main/java/com/Hiran/com/pruebaSimulador/App.java
+++ src/main/java/com/Hiran/com/pruebaSimulador/App.java
@@ -12,9 +12,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
 	   
-    		
+	   
     public static void main( String[] args ){
     WebDriver driver;  
+    
+    By perfil			= By.name("perfil");
+	By valorAplicar   	= By.id("valorAplicar");
+    By valorInvestir  	= By.id("valorInvestir");
+    By tempo       		= By.id("tempo");
+    By simular     		= By.xpath("//*[@id=\"formInvestimento\"]/div[5]/ul/li[2]/button");  
+    					  
+    By btnRefazer 		= By.xpath("/html/body/div[3]/div/div/div[1]/div/div[2]/a");
+    By anos				= By.xpath("//*[@id=\"formInvestimento\"]/div[4]/div[2]/div[2]/ul/li[2]/a");
+    By dropdown			= By.xpath("//*[@id=\"formInvestimento\"]/div[4]/div[2]/div[2]/a/span[4]");
+    
+    
     
       //Configurando webdriver
       System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
@@ -25,40 +37,90 @@ public class App {
 	  JavascriptExecutor jse = (JavascriptExecutor)driver;
 	  jse.executeScript("window.scrollBy(0,500)");
   	 
+     	    	  
+	 
+	  //Perfil persona espacios en blanco
+	  
+	  //public void testUno() {
+	  driver.findElement(simular).click();
+	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	  driver.navigate().refresh();	
+	  
 	 
 	//Perfil persona-inversion minima- ahorro minimo-tiempo minimo
-	driver.findElement(By.name("perfil")).click(); 
-  	driver.findElement(By.id("valorAplicar")).sendKeys("2000");
-  	driver.findElement(By.id("valorInvestir")).sendKeys("2000");
-  	driver.findElement(By.id("tempo")).sendKeys("1");
-  	driver.findElement(By.xpath("//*[@id=\"formInvestimento\"]/div[5]/ul/li[2]/button")).click();
+	driver.findElement(perfil).click(); 
+  	driver.findElement(valorAplicar).sendKeys("2000");
+  	driver.findElement(valorInvestir).sendKeys("2000");
+  	driver.findElement(tempo).sendKeys("1");
+  	//simular
+  	driver.findElement(simular).click();
   	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-  	//submit
-  	driver.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/div/div[2]/a")).click();
+  	// Rehacer simulacion                          
+  	driver.findElement(btnRefazer).click();
   	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+  	
 	  
-  	
-  	//Perfil persona- inversion maxima- ahorro maximo- tiempo maximo
-  	driver.findElement(By.name("perfil")).click();
-  	driver.findElement(By.id("valorAplicar")).sendKeys ("9999999900");
-  	driver.findElement(By.id("valorInvestir")).sendKeys("9999999900");
-  	driver.findElement(By.id("tempo")).sendKeys("99");
-  	driver.findElement(By.xpath("//*[@id=\"formInvestimento\"]/div[4]/div[2]/div[2]/a/span[4]")).click();
-  	//selecciona años
-  	driver.findElement(By.xpath("//*[@id=\"formInvestimento\"]/div[4]/div[2]/div[2]/ul/li[2]/a")).click();
-  	driver.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/div/div[2]/a")).click();                           
-  	//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-  	
-  	//driver.findElement(By.xpath("//*[@id=\"formInvestimento\"]/div[5]/ul/li[1]/a")).click();
-  	//driver.findElement(By.xpath("/html/body/div[4]/div/div/div[1]/div/div[1]/form/div[5]/ul/li[1]/a")).click();
-  	//driver.findElement(By.className("btn btnLimpar")).click();
-  	driver.findElement(By.tagName("btn btnLimpar")).click();
-  	//driver.navigate().refresh();
+	  //Perfil persona- inversion maxima- ahorro maximo- tiempo maximo
+	  	driver.findElement(perfil).click();
+	  	driver.findElement(valorAplicar).sendKeys ("999999900");
+	  	driver.findElement(valorInvestir).sendKeys("999999900");
+	  	driver.findElement(tempo).sendKeys("99");
+	  	//click dropdown
+	  	driver.findElement(dropdown).click();
+	  	//selecciona años
+	   	driver.findElement(anos).click();
+	  	//simular
+	  	driver.findElement(simular).click();
+	  		
+	  	driver.navigate().refresh();
+	  
+	  	     	
   	
   	
-  	//
+  	    //Perfil persona- inversion superior maxima- ahorro superior maximo- tiempo superior maximo
+	  	driver.findElement(perfil).click();
+	  	driver.findElement(valorAplicar).sendKeys ("999999901");
+	  	driver.findElement(valorInvestir).sendKeys("999999901");
+	  	driver.findElement(tempo).sendKeys("100");
+	  	//dropdown
+	  	driver.findElement(dropdown).click();
+	  	//selecciona años
+	  	driver.findElement(anos).click();
+	  	//simular
+	  	driver.findElement(simular).click();
+	  	driver.navigate().refresh();
+  	  
+	  	
+	  	  
+	     //Perfil persona- caracteres raros meses
+	  	driver.findElement(perfil).click();
+	  	driver.findElement(valorAplicar).sendKeys ("$%&");
+	  	driver.findElement(valorInvestir).sendKeys("$%&");
+	  	driver.findElement(tempo).sendKeys("$%&");
+	  	//simular
+	  	driver.findElement(simular).click();
+	  	driver.navigate().refresh();
+	  
+	  	
+	  	 //Perfil persona- caracteres raros años
+	  	driver.findElement(perfil).click();
+	  	driver.findElement(valorAplicar).sendKeys ("$%&");
+	  	driver.findElement(valorInvestir).sendKeys("$%&");
+	  	driver.findElement(tempo).sendKeys("$%&");
+	  	//dropdown
+	  	driver.findElement(dropdown).click();
+	  	//selecciona años
+	  	driver.findElement(anos).click();
+	  	//simular
+	  	driver.findElement(simular).click();
+	  	driver.navigate().refresh();
+	  
     }
-    }  
+}
+  	
+  	
+
+     
 
 
     
--- src/test/java/com/Hiran/com/pruebaSimulador/AppTest.java
+++ src/test/java/com/Hiran/com/pruebaSimulador/AppTest.java
@@ -2,17 +2,28 @@ package com.Hiran.com.pruebaSimulador;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+
     /**
      * Rigorous Test :-)
      */
+			
+	
     @Test
+        
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
