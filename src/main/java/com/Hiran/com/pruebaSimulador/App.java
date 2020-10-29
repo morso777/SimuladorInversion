9e963547d59dac849cad5426195125d0130fb0d5
hiran peña toledo <vohiran@hotmail.com>
2020-10-29 09:21:54 -0600

    3 commit

--- src/main/java/com/Hiran/com/pruebaSimulador/App.java
+++ src/main/java/com/Hiran/com/pruebaSimulador/App.java
@@ -2,25 +2,20 @@ package com.Hiran.com.pruebaSimulador;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
-	
-	By perfil			= By.name("perfil");
-	By valorAplicar   	= By.id("valorAplicar");
-    By valorInvestir  	= By.id("valorInvestir");
-    By Tempo       		= By.id("tempo");
-    By simular     		= By.xpath("//*[@id=\"formInvestimento\"]/div[5]/ul/li[2]/button");
-    By btnRefazer 		= By.xpath("/html/body/div[4]/div/div/div[1]/div/div[2]/a");
-    
-    
+	   
     		
     public static void main( String[] args ){
     WebDriver driver;  
-    	
+    
       //Configurando webdriver
       System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
   	  driver = new ChromeDriver ();
@@ -30,33 +25,42 @@ public class App {
 	  JavascriptExecutor jse = (JavascriptExecutor)driver;
 	  jse.executeScript("window.scrollBy(0,500)");
   	 
-	  //Perfil persona-inversion minima- ahorro minimo-tiempo minimo
-  	driver.findElement(By.name("perfil")).click();
+	 
+	//Perfil persona-inversion minima- ahorro minimo-tiempo minimo
+	driver.findElement(By.name("perfil")).click(); 
   	driver.findElement(By.id("valorAplicar")).sendKeys("2000");
   	driver.findElement(By.id("valorInvestir")).sendKeys("2000");
   	driver.findElement(By.id("tempo")).sendKeys("1");
   	driver.findElement(By.xpath("//*[@id=\"formInvestimento\"]/div[5]/ul/li[2]/button")).click();
   	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-  	
-  	driver.findElement(By.xpath("/html/body/div[4]/div/div/div[1]/div/div[2]/a"));  	
+  	//submit
+  	driver.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/div/div[2]/a")).click();
   	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	  
   	
-  	/*
-  	 //Perfil persona- inversion maxima- ahorro maximo- tiempo maximo
+  	//Perfil persona- inversion maxima- ahorro maximo- tiempo maximo
   	driver.findElement(By.name("perfil")).click();
-  	driver.findElement(By.id("valorAplicar")).sendKeys("2000");
-  	driver.findElement(By.id("valorInvestir")).sendKeys("2000");
+  	driver.findElement(By.id("valorAplicar")).sendKeys ("9999999900");
+  	driver.findElement(By.id("valorInvestir")).sendKeys("9999999900");
   	driver.findElement(By.id("tempo")).sendKeys("99");
-  	driver.findElement(By.name("seta")).click();
-  	driver.findElement(By.xpath ("//*[@id=\"formInvestimento\"]/div[4]/div[2]/div[2]/ul/li[2]/a"));
-  	driver.findElement(By.xpath("//*[@id=\"formInvestimento\"]/div[5]/ul/li[2]/button")).click();
-  	 
-  	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);*/
+  	driver.findElement(By.xpath("//*[@id=\"formInvestimento\"]/div[4]/div[2]/div[2]/a/span[4]")).click();
+  	//selecciona años
+  	driver.findElement(By.xpath("//*[@id=\"formInvestimento\"]/div[4]/div[2]/div[2]/ul/li[2]/a")).click();
+  	driver.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/div/div[2]/a")).click();                           
+  	//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
   	
+  	//driver.findElement(By.xpath("//*[@id=\"formInvestimento\"]/div[5]/ul/li[1]/a")).click();
+  	//driver.findElement(By.xpath("/html/body/div[4]/div/div/div[1]/div/div[1]/form/div[5]/ul/li[1]/a")).click();
+  	//driver.findElement(By.className("btn btnLimpar")).click();
+  	driver.findElement(By.tagName("btn btnLimpar")).click();
+  	//driver.navigate().refresh();
+  	
+  	
+  	//
     }
-     
+    }  
 
-}
+
     
 
 
